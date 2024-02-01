4d4668d340b7e0819eb342a8c63ac0f9640c2276
EricDuong <ericlamduong@gmail.com>
2024-02-01 17:59:53 -0600

    refactor nav images to use exports from Images.ts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <ul className={styles.list}>
-          <li className={styles.logo}><a className={styles.icon} href='#'><img src='../images/Logo 2024.png'/></a></li>
+          <li className={styles.logo}><a className={styles.icon} href='#'><img src={picklogo24.src}/></a></li>
           <li className={styles.item}><a className={styles.buttons} href='#schedule'>Schedule</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#sponsors'>Sponsors</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#team'>Team</a></li>
@@ -15,11 +15,11 @@ function Navbar() {
           <li className={styles.apply}><a className={styles.buttons} href='https://www.pickhacks.io'>APPLY TODAY!</a></li>
         </ul>
         <ul className={styles.social}>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src='../images/IconDiscord.png'/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src='../images/IconInstagram.png'/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src='../images/IconLinkedIn.png'/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src='../images/IconTikTok.png'/></a></li>
-          <li className={styles.banneritem}><a className={styles.banner} href='#'><img src='../images/MLH Banner.png'/></a></li>
+          <li className={styles.item}><a className={styles.icon} href='#'><img src={discord.src}/></a></li>
+          <li className={styles.item}><a className={styles.icon} href='#'><img src={instagram.src}/></a></li>
+          <li className={styles.item}><a className={styles.icon} href='#'><img src={linkedin.src}/></a></li>
+          <li className={styles.item}><a className={styles.icon} href='#'><img src={tiktok.src}/></a></li>
+          <li className={styles.banneritem}><a className={styles.banner} href='#'><img src={mlh.src}/></a></li>
         </ul>
       </div>
     </nav>
