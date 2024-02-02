17837bd977b25c18714f20c8d1da6901b432673c
Everett Yan <everettzyan@gmail.com>
2024-02-01 18:17:26 -0600

    Updated Navbar
    FIX FONT

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,19 @@ function Navbar() {
       <div className={styles.container}>
         <ul className={styles.list}>
           <li className={styles.logo}><a className={styles.icon} href='#'><img src={picklogo24.src}/></a></li>
+          <li className={styles.item}><a className={styles.buttons} href='#'>Home</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#schedule'>Schedule</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#sponsors'>Sponsors</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#team'>Team</a></li>
           <li className={styles.item}><a className={styles.buttons} href='#faq'>FAQ</a></li>
-          <li className={styles.apply}><a className={styles.buttons} href='https://www.pickhacks.io'>APPLY TODAY!</a></li>
+          <li className={styles.apply}><a className={styles.applybutton} href='https://www.pickhacks.io'>REGISTER</a></li>
         </ul>
         <ul className={styles.social}>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src={discord.src}/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src={instagram.src}/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src={linkedin.src}/></a></li>
-          <li className={styles.item}><a className={styles.icon} href='#'><img src={tiktok.src}/></a></li>
-          <li className={styles.banneritem}><a className={styles.banner} href='#'><img src={mlh.src}/></a></li>
+          <li className={styles.socialitem}><a className={styles.icon} href='https://discord.gg/p3fcxGzjxa'><img src={discord.src}/></a></li>
+          <li className={styles.socialitem}><a className={styles.icon} href='https://www.instagram.com/sandtpickhacks'><img src={instagram.src}/></a></li>
+          <li className={styles.socialitem}><a className={styles.icon} href='https://www.linkedin.com/company/pickhacks'><img src={linkedin.src}/></a></li>
+          <li className={styles.socialitem}><a className={styles.icon} href='#'><img src={tiktok.src}/></a></li>
+          <li className={styles.banneritem}><a className={styles.banner} href='https://mlh.io/'><img src={mlh.src}/></a></li>
         </ul>
       </div>
     </nav>
