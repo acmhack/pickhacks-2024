ecfb50f7124b596fae36bd567673edc4cea34c24
EricDuong <ericlamduong@gmail.com>
2024-02-22 15:56:39 -0600

    init sponsor info data

---
+++ src/lib/Sponsors.ts
@@ -0,0 +1,10 @@
+import { ISponsor } from "@/interfaces/ISponsor";
+import { littlestGuy } from "./Images";
+
+export const SponsorInfo: ISponsor[] = [
+    {
+        name: 'test',
+        tier: 'gold',
+        logo: littlestGuy
+    }
+]
