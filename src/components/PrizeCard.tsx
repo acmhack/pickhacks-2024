7793c012c981b1a37331c1e7ddbebd07894a1e49
ewbyf <107148516+ewbyf@users.noreply.github.com>
2024-02-22 19:10:58 -0600

    changed how prizes carousel looks

---
+++ src/components/PrizeCard.tsx
@@ -0,0 +1,22 @@
+import styles from '@/styles/components/PrizeCard.module.css';
+
+interface IProps {
+	title: string;
+	description: string;
+	image: string;
+	link: string;
+}
+
+const PrizeCard = ({ title, description, image, link }: IProps) => {
+	return (
+		<div className={styles.box}>
+			<a className={styles.cardContainer} href={link} target='_blank'>
+				<p className={styles.title}>{title}</p>
+				<p className={styles.description}>{description}</p>
+				<img src={image} className={styles.image} />
+			</a>
+		</div>
+	);
+};
+
+export default PrizeCard;
--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -1,111 +1,75 @@
-import * as React from 'react';
 import styles from '../styles/components/Prizes.module.css';
 
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import useMeasure from 'react-use-measure';
-import { IPrize } from '@/interfaces/IPrize';
 import { PrizeList } from '@/lib/PrizeList';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import PrizeCard from './PrizeCard';
 
-const CARD_WIDTH = 300;
-const CARD_HEIGHT = 300;
-const MARGIN = 40;
-const CARD_SIZE = CARD_WIDTH + MARGIN;
+function PrevArrow(props: any) {
+	const { className, style, onClick } = props;
+	return (
+		<div className={styles.leftButton} style={{ ...style, display: 'block' }} onClick={onClick}>
+			<FiChevronLeft />
+		</div>
+	);
+}
 
-const BREAKPOINTS = {
-	sm: 640,
-	lg: 1024,
-};
+function NextArrow(props: any) {
+	const { className, style, onClick } = props;
+	return (
+		<div className={styles.rightButton} style={{ ...style, display: 'block' }} onClick={onClick}>
+			<FiChevronRight />
+		</div>
+	);
+}
 
 const Prizes = () => {
-	const [ref, { width }] = useMeasure();
-	const [offset, setOffset] = useState(0);
-
-	const CARD_BUFFER = width > BREAKPOINTS.lg ? 3 : width > BREAKPOINTS.sm ? 2 : 1;
-
-	const CAN_SHIFT_LEFT = offset < 0;
-
-	const CAN_SHIFT_RIGHT = Math.abs(offset) < CARD_SIZE * (PrizeList.length - CARD_BUFFER);
-
-	const shiftLeft = () => {
-		if (!CAN_SHIFT_LEFT) {
-			return;
-		}
-		setOffset((pv) => (pv += CARD_SIZE));
+	var settings = {
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		speed: 1500,
+		arrows: true,
+		initialSlide: 0,
+		centerPadding: '0px',
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
+		responsive: [
+			{
+				breakpoint: 1650,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},
+			{
+				breakpoint: 1250,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 825,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	};
-
-	const shiftRight = () => {
-		if (!CAN_SHIFT_RIGHT) {
-			return;
-		}
-		setOffset((pv) => (pv -= CARD_SIZE));
-	};
-
 	return (
-		<section ref={ref}>
-			<div className={styles.container}>
-				{/* CARDS */}
-				<div className={styles.row}>
-					<motion.div
-						animate={{
-							x: offset,
-						}}
-                        style={{
-                            display: 'flex',
-                        }}
-					>
-						{PrizeList.map((item) => {
-							return <Card key={item.id} {...item} />;
-						})}
-					</motion.div>
-				</div>
-
-				{/* BUTTONS */}
-				<>
-					<motion.button
-						initial={false}
-						animate={{
-							x: CAN_SHIFT_LEFT ? '0%' : '-100%',
-						}}
-						className={styles.leftButton}
-						onClick={shiftLeft}
-					>
-						<FiChevronLeft />
-					</motion.button>
-					<motion.button
-						initial={false}
-						animate={{
-							x: CAN_SHIFT_RIGHT ? '0%' : '100%',
-						}}
-						className={styles.rightButton}
-						onClick={shiftRight}
-					>
-						<FiChevronRight />
-					</motion.button>
-				</>
+		<>
+			<div className={styles.carouselContainer}>
+				<Slider {...settings} className={styles.inner}>
+					{PrizeList.map((item) => (
+						<PrizeCard key={item.id} title={item.title} description={item.description} image={item.img} link={item.link}></PrizeCard>
+					))}
+				</Slider>
 			</div>
-		</section>
-	);
-};
-
-const Card = ({ img, title, description, link }: IPrize) => {
-	return (
-		<a
-			className={styles.cardContainer}
-			style={{
-				width: CARD_WIDTH,
-				height: CARD_HEIGHT,
-				marginRight: MARGIN,
-				backgroundColor: '#2A1449'
-			}}
-            href={link}
-            target='_blank'
-		>
-            <p className={styles.title}>{title}</p>
-            <p className={styles.description}>{description}</p>
-            <img src={img} className={styles.image}></img>
-		</a>
+		</>
 	);
 };
 
