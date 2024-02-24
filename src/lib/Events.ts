814b79af978126e6c1b20ab653c059d5bd8886b3
Everett Yan <everettzyan@gmail.com>
2024-02-23 21:47:12 -0600

    updated prizes and events list

--- src/lib/Events.ts
+++ src/lib/Events.ts
@@ -4,58 +4,148 @@ export const Events: IEvent[] = [
     // Friday
     {
         day: 'Friday',
-        event: 'Breakfast',
-        time: '8:00am',
+        event: 'Sponsor Check-in',
+        time: '2:00PM',
         location: 'Location TBA'
     },
     {
         day: 'Friday',
-        event: 'Company Talk',
-        time: '11:00am',
+        event: 'Hacker Check-in',
+        time: '4:00PM',
         location: 'Location TBA'
     },
     {
         day: 'Friday',
-        event: 'Lunch',
-        time: '12:00pm',
+        event: 'Opening Ceremony',
+        time: '6:00PM-7:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Friday',
+        event: 'Hacking Begins',
+        time: '~7:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Friday',
+        event: 'Team Matching',
+        time: '~7:00PM-7:30PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Friday',
+        event: 'Dinner',
+        time: '7:30PM-8:30PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Friday',
+        event: 'Casino Night',
+        time: '9:00PM-11:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Friday',
+        event: 'Night Snack',
+        time: '10:00PM',
         location: 'Location TBA'
     },
     // Saturday
     {
         day: 'Saturday',
         event: 'Breakfast',
-        time: '8:00am',
+        time: '8:00AM-10:00AM',
         location: 'Location TBA'
     },
     {
         day: 'Saturday',
-        event: 'Company Talk',
-        time: '11:00am',
+        event: 'Speaker 1',
+        time: '11:00AM-12:00PM',
         location: 'Location TBA'
     },
     {
         day: 'Saturday',
         event: 'Lunch',
-        time: '12:00pm',
+        time: '12:30PM-1:30PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Saturday',
+        event: 'MLH CTF',
+        time: '2:00PM-3:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Saturday',
+        event: 'Dinner',
+        time: '6:00PM-8:00pm',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Saturday',
+        event: 'ACM Security',
+        time: '7:30PM-8:30PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Saturday',
+        event: 'MLH Bob Ross',
+        time: '9:00PM-10:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Saturday',
+        event: 'Night Snack',
+        time: '10:00PM',
         location: 'Location TBA'
     },
     // Sunday
     {
         day: 'Sunday',
         event: 'Breakfast',
-        time: '8:00am',
+        time: '8:00AM-10:00AM',
         location: 'Location TBA'
     },
     {
         day: 'Sunday',
-        event: 'Company Talk',
-        time: '11:00am',
+        event: 'Submission - Soft Deadline',
+        time: '10:00AM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Sunday',
+        event: 'Hacking Ends',
+        time: '11:00AM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Sunday',
+        event: 'Judging',
+        time: '11:00AM-1:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Sunday',
+        event: 'Speaker 2',
+        time: '10:30AM-11:15AM',
         location: 'Location TBA'
     },
     {
         day: 'Sunday',
         event: 'Lunch',
-        time: '12:00pm',
+        time: '12:00PM-2:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Sunday',
+        event: 'Closing Ceremony',
+        time: '2:00PM-3:00PM',
+        location: 'Location TBA'
+    },
+    {
+        day: 'Sunday',
+        event: 'Venue Closes for Hackers/Sponsors',
+        time: '4:00PM',
         location: 'Location TBA'
     },
   ];
