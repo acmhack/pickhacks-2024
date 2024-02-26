81424304096da5f649eb2ccc18b2e71df5cbfc0f
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2024-02-26 16:46:22 -0600

    faq update

--- src/components/FaqAccordian.tsx
+++ src/components/FaqAccordian.tsx
@@ -3,44 +3,46 @@ import { bullet, bulletExpand } from '@/lib/Images';
 import styles from '@/styles/components/Faq.module.css';
 
 type Props = {
-    question: string,
-    answer: string,
-    turn: boolean[],
-    setTurn: Dispatch<SetStateAction<boolean[]>>,
-    idx: number
-}
+	question: string;
+	answer: string;
+	turn: boolean[];
+	setTurn: Dispatch<SetStateAction<boolean[]>>;
+	idx: number;
+};
 
-const FaqAccordian = ({question, answer, turn, setTurn, idx}: Props) => {
+const FaqAccordian = ({ question, answer, turn, setTurn, idx }: Props) => {
+	const toggleAccordian = () => {
+		let newTurn = [...turn!];
+		newTurn[idx] = !newTurn[idx];
+		setTurn!(newTurn);
+	};
 
-    const toggleAccordian = () => {
-        let newTurn = [...turn!];
-        newTurn[idx] = !newTurn[idx]
-        setTurn!(newTurn)
-    }
-
-    return (
-        <div>
-            <button onClick={toggleAccordian}
-            className={styles['faq-button']}
-            >
-            <div className={styles['faq-bullet']}>
-                { turn![idx] ?
-                <img className={styles['bullet-img']} src={bulletExpand.src} alt=""/> :
-                <img className={styles['bullet-img']} src={bullet.src} alt=""/>
-                }
-                <p className={styles['faq-question']}>{question}</p>
-            </div>
-            </button>
-            <div className={styles['faq-answer']}
-            style={
-                turn![idx]
-                ? { maxHeight: "250px" } : // bc of verbose answers 60px -> 250px
-                { maxHeight: "0"}
-            }>
-                <p>{answer}</p>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div style={{margin: '1em 0'}}>
+			<button onClick={toggleAccordian} className={styles['faq-button']}>
+				<div className={styles['faq-bullet']}>
+					{turn![idx] ? (
+						<img className={styles['bullet-img']} src={bulletExpand.src} alt="" />
+					) : (
+						<img className={styles['bullet-img']} src={bullet.src} alt="" />
+					)}
+					<p className={styles['faq-question']}>{question}</p>
+				</div>
+			</button>
+			<div
+				className={styles.answerContainer}
+				style={
+					turn![idx]
+						? { gridTemplateRows: '1fr' } // bc of verbose answers 60px -> 250px
+						: { gridTemplateRows: '0fr' }
+				}
+			>
+				<div className={styles.answer}>
+					<p>{answer}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default FaqAccordian;
--- src/lib/FaqData.ts
+++ src/lib/FaqData.ts
@@ -1,44 +1,44 @@
-import { IQA } from "@/interfaces/IQA";
+import { IQA } from '@/interfaces/IQA';
 
 export const QAData: IQA[] = [
-    {
-        idx: 0,
-        question: "What is PickHacks?",
-        answer: "PickHacks is Missouri S&T's annual hackathon — an innovation competition where students come together to bring a project to life over the course of a weekend. You come up with the idea, and we provide mentors, resources, and anything else you need. Programming experience isn't necessary; the only requirement is a willingness to learn and have fun! Plus, it's FREE!"
-    },
-    {
-        idx: 1,
-        question: "Where",
-        answer: "The event will be happening in the Innovation Center here on the Missouri S&T campus!"
-    },
-    {
-        idx: 2,
-        question: "When",
-        answer: "PickHacks is taking place the weekend of April 14th through 16th! (possibly innacurate)"
-    },
-    {
-        idx: 3,
-        question: "Who is this for",
-        answer: "Anyone currently enrolled in an educational institution (high school and above) or 12 months out of graduation is able to attend. You don't even have to know how to code to join! Just come in with an open mind and fun ideas to expand on, and you'll be set!"
-    },
-    {
-        idx: 4,
-        question: "How big can my team be?",
-        answer: "5 (I don't know)"
-    },
-    {
-        idx: 5,
-        question: "Is it free?",
-        answer: "It is free"
-    },
-    {
-        idx: 6,
-        question: "What if I've never been?",
-        answer: "Hackathons are a fun way to learn new skills that you can add to your resume and connect with sponsors and other students while competing for prizes. We will have free swag at the event for anyone who comes! Additionally, there will be ample food provided for FREE throughout the weekend!"
-    },
-    {
-        idx: 7,
-        question: "Will there be places to sleep?",
-        answer: "Yes, in the tunnels."
-    }
-]
+	{
+		idx: 0,
+		question: 'What is PickHacks?',
+		answer: "PickHacks is Missouri S&T's annual hackathon — an innovation competition where students come together to bring a project to life over the course of a weekend. You come up with the idea, and we provide mentors, resources, and anything else you need. Programming experience isn't necessary; the only requirement is a willingness to learn and have fun!",
+	},
+	{
+		idx: 1,
+		question: 'Will PickHacks happen in person or virtually?',
+		answer: 'PickHacks will be a fully in-person hackathon, hosted at the Innovation Center here on the Missouri S&T campus! We will not be offering a virtual attendance option this year.',
+	},
+	{
+		idx: 2,
+		question: 'When will PickHacks take place?',
+		answer: 'PickHacks is taking place the weekend of April 5th through 7th!',
+	},
+	{
+		idx: 3,
+		question: 'Who is elgible to apply?',
+		answer: "Anyone who is 18+ and currently enrolled at a college or university is able to attend! You don't even have to know how to code to join! Just come in with an open mind and fun ideas to expand on, and you'll be set!",
+	},
+	{
+		idx: 4,
+		question: 'How big can my team be?',
+		answer: 'A team can consist of 1-4 total members.',
+	},
+	{
+		idx: 5,
+		question: 'How much does it cost to attend PickHacks?',
+		answer: 'Admission to PickHacks is completely free! We will also provide food and accommodation for the entire event, as well as all the resources and mentorship our hackers need to build something incredible.',
+	},
+	{
+		idx: 6,
+		question: "What if I've never been to a hackathon?",
+		answer: 'We welcome students of all skill levels and majors. Hackathons are a fun way to learn new skills that you can add to your resume and connect with sponsors and other students while competing for prizes. We will have free swag at the event for anyone who comes! Additionally, there will be ample food provided for FREE throughout the weekend!',
+	},
+	{
+		idx: 7,
+		question: 'Will there be places to sleep?',
+		answer: "Yes! We'll have designated places to sleep along with some limited air mattresses. Just make sure to bring a pillow and blanket!",
+	},
+];
