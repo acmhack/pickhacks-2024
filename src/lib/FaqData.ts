4b568491e4cc4e7a9085950aefaf22a7ccfffdd0
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2024-02-28 15:03:11 -0600

    faq update

--- src/lib/FaqData.ts
+++ src/lib/FaqData.ts
@@ -8,8 +8,8 @@ export const QAData: IQA[] = [
 	},
 	{
 		idx: 1,
-		question: 'Will PickHacks happen in person or virtually?',
-		answer: 'PickHacks will be a fully in-person hackathon, hosted at the Innovation Lab here on the Missouri S&T campus! We will not be offering a virtual attendance option this year.',
+		question: 'Can I participate virtually?',
+		answer: 'Unfortunately, we will not be offering a virtual attendance option this year. PickHacks 2024 will be fully held in-person at our brand new Innovation Lab to create an interactive environment and encourage everyone to network with our sponsors and meet new people.',
 	},
 	{
 		idx: 2,
@@ -23,8 +23,8 @@ export const QAData: IQA[] = [
 	},
 	{
 		idx: 4,
-		question: 'How big can my team be?',
-		answer: 'A team can consist of 1-4 total members.',
+		question: 'How big can my team be? Do I need a team?',
+		answer: "Each team can consist of up to 4 total members. Having a team isn't necessary to participate, but it's highly encouraged for you to form or join a team because it will transform the hackathon into an extremely fun and memorable experience! If you are interested in forming or joining a team, reach out in our discord. If you don't have a team by the hackathon and are interested in one, don't worry! We will be matching people with similar interests together for teams."
 	},
 	{
 		idx: 5,
@@ -39,6 +39,6 @@ export const QAData: IQA[] = [
 	{
 		idx: 7,
 		question: 'Will there be places to sleep?',
-		answer: "Yes! We'll have designated places to sleep along with some limited air mattresses. Just make sure to bring a pillow and blanket!",
+		answer: "Yes! If you're not from S&T, we will have designed sleeping areas at the hackathon! We will only have a few air mattresses, so make sure to bring your own sleeping bags if you can! You can also choose to rent your own room and stay at a hotel if that's easier or more comfortable for you.",
 	},
 ];
