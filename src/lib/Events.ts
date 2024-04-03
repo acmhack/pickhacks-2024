6e8d4d346c1c3c55e3ac18bc3d42fb9f877362ad
EricDuong <ericlamduong@gmail.com>
2024-04-03 15:02:53 -0500

    update event data

--- src/lib/Events.ts
+++ src/lib/Events.ts
@@ -6,128 +6,134 @@ export const Events: IEvent[] = [
         day: 'Friday',
         event: 'Hacker Check-in',
         time: '4:00pm',
-        location: 'Location TBA'
+        location: 'Innovation Lab - Entrance'
     },
     {
         day: 'Friday',
         event: 'Opening Ceremony',
         time: '6:00pm - 7:00pm',
-        location: 'Location TBA'
+        location: 'Lobby and Multipurpose Room 120'
     },
     {
         day: 'Friday',
         event: 'Hacking Begins',
         time: '7:00pm',
-        location: 'Location TBA'
+        location: 'Innovation Lab'
     },
     {
         day: 'Friday',
         event: 'Team Matching',
         time: '7:00pm - 7:30pm',
-        location: 'Location TBA'
+        location: 'Room 115'
     },
     {
         day: 'Friday',
         event: 'Dinner',
         time: '7:30pm - 8:30pm',
-        location: 'Location TBA'
+        location: 'Havener - St. Pats A, B, C'
     },
     {
         day: 'Friday',
-        event: 'Event TBA',
+        event: 'Casino Night',
         time: '9:00pm - 11:00pm',
-        location: 'Location TBA'
+        location: 'Room 115'
     },
     {
         day: 'Friday',
-        event: 'Night Snack',
+        event: 'Late Night Snack',
         time: '10:00pm',
-        location: 'Location TBA'
+        location: 'Havener - Shamrock'
     },
     // Saturday
     {
         day: 'Saturday',
         event: 'Breakfast',
-        time: '8:00am - 10:00am',
-        location: 'Location TBA'
+        time: '8:00am - 10:00am', //9:30am
+        location: 'Havener - St. Pats A, B, C'
     },
     {
         day: 'Saturday',
-        event: 'Event TBA',
-        time: '11:00am - 12:00am',
-        location: 'Location TBA'
+        event: 'Speaker',
+        time: '11:00am - 12:00pm',
+        location: 'Room 115'
     },
     {
         day: 'Saturday',
         event: 'Lunch',
         time: '12:30pm - 1:30pm',
-        location: 'Location TBA'
+        location: 'Havener - St. Pats A, B, C'
     },
     {
         day: 'Saturday',
-        event: 'Event TBA',
+        event: 'MLH GitHub Copilot',
         time: '2:00pm - 3:00pm',
-        location: 'Location TBA'
+        location: 'Room 115'
+    },
+    {
+        day: 'Saturday',
+        event: 'ACM-AI and GDSC Presentations',
+        time: '4:00pm - 5:00pm',
+        location: 'ACM-AI in Room 115; GDSC in Room 213'
     },
     {
         day: 'Saturday',
         event: 'Dinner',
         time: '6:00pm - 8:00pm',
-        location: 'Location TBA'
+        location: 'Havener - St. Pats A, B, C'
     },
     {
         day: 'Saturday',
-        event: 'Event TBA',
-        time: '7:30pm - 8:30pm',
-        location: 'Location TBA'
+        event: 'Bob Ross Event',
+        time: '7:00pm',
+        location: 'Room 115'
     },
     {
         day: 'Saturday',
-        event: 'Event TBA',
-        time: '9:00pm - 10:00pm',
-        location: 'Location TBA'
-    },
-    {
-        day: 'Saturday',
-        event: 'Night Snack',
-        time: '10:00PM',
-        location: 'Location TBA'
+        event: 'Late Night Snack',
+        time: '10:00pm',
+        location: 'Havener - Shamrock'
     },
     // Sunday
     {
         day: 'Sunday',
         event: 'Breakfast',
         time: '8:00am - 10:00am',
-        location: 'Location TBA'
+        location: 'Havener - St. Pats A, B, C'
+    },
+    {
+        day: 'Sunday',
+        event: 'Project Submissions Due',
+        time: '10am',
+        location: 'Online - Devpost'
     },
     {
         day: 'Sunday',
         event: 'Hacking Ends',
-        time: '11:00am',
-        location: 'Location TBA'
+        time: '11am',
+        location: 'Online - Devpost'
     },
     {
         day: 'Sunday',
-        event: 'Judging',
-        time: '11:00am -1:00pm',
-        location: 'Location TBA'
+        event: 'Travel Reimbursement Booth',
+        time: '10:00am - 2:00pm',
+        location: 'Innovation Lab 1st Floor'
     },
     {
         day: 'Sunday',
-        event: 'Event TBA',
-        time: '10:30am - 11:15am',
-        location: 'Location TBA'
+        event: 'Competitive Games',
+        time: '10:00am - 12:00pm',
+        location: 'Room 214'
     },
     {
         day: 'Sunday',
         event: 'Lunch',
         time: '12:00pm - 2:00pm',
-        location: 'Location TBA'
+        location: 'Havener - Meramec-Gasconade'
     },
     {
         day: 'Sunday',
         event: 'Closing Ceremony',
         time: '2:00pm - 3:00pm',
-        location: 'Location TBA'
+        location: 'Lobby and Multipurpose Room 120'
     },
   ];
