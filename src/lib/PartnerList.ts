656fc44e5b6b783fee3e341c1d37c61bc6653409
ewbyf <107148516+ewbyf@users.noreply.github.com>
2024-04-03 23:03:34 -0500

    partner

--- src/lib/PartnerList.ts
+++ src/lib/PartnerList.ts
@@ -1,21 +1,18 @@
-import { ISponsor } from "@/interfaces/ISponsor";
-import { mosystems, mlh, standoutstickers, stuco } from "./Images";
+import { ISponsor } from '@/interfaces/ISponsor';
+import { mosystems, mlh, standoutstickers, stuco } from './Images';
 
-export const PartnerList: ISponsor[] = [ //placeholder links
-    {
-        logo: mlh.src,
-        link: 'https://mlh.io/'
-    },
-    {
-        logo: standoutstickers.src,
-        link: 'https://www.standoutstickers.com/'
-    },
-    {
-        logo: mosystems.src,
-        link: 'https://www.umsystem.edu/'
-    },
-    {
-        logo: stuco.src,
-        link: 'https://stuco.mst.edu/'
-    },
-]
+export const PartnerList: ISponsor[] = [
+	//placeholder links
+	{
+		logo: mlh.src,
+		link: 'https://mlh.io/'
+	},
+	{
+		logo: mosystems.src,
+		link: 'https://www.umsystem.edu/'
+	},
+	{
+		logo: stuco.src,
+		link: 'https://stuco.mst.edu/'
+	}
+];
